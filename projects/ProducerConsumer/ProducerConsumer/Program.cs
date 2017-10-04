3503fb281f658c05016658afc5a04b666a940f80
kasp5953 <kasp5953@edu.easj.dk>
2017-10-04 08:40:48 +0200

    only 3.9 missing

---
+++ projects/ProducerConsumer/ProducerConsumer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+#pragma warning disable 4014
+
+namespace ProducerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sets up and starts a scenario
+            Scenario theScenario = new Scenario(40, 20, 40, 100, 80, Reporter<Data>.ReportMode.verbose);
+            theScenario.RunAsync();
+
+            Console.WriteLine("Press any key to abort the run...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ projects/SlicesOfPi/SlicesOfPi/PiCalcAlgorithm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SlicesOfPi
+{
+    /// <summary>
+    /// This class implements the algorithm for calculating
+    /// an approximate value of Pi.
+    /// </summary>
+    public class PiCalcAlgorithm
+    {
+        /// <summary>
+        /// Calculates approximate value of Pi. The algorithm is
+        /// incremental, i.e. the approximate value get better and
+        /// better, the longer the algorithm runs.
+        /// Note that this version does NOT use async.
+        /// </summary>
+        /// <param name="iterations">Number of iterations to perform</param>
+        /// <returns>Final value of Pi</returns>
+        public double Calculate(int iterations)
+        {
+            // Setup 
+            Random _generator = new Random();
+            int insideUnitCircle = 0;
+
+            // Main loop in algorithm
+            for (long i = 1; i <= iterations; i++)
+            {
+                insideUnitCircle += RandomPointWithinUnitCircle(_generator) ? 1 : 0;
+            }
+
+            // Return final value of Pi 
+            return insideUnitCircle * 4.0 / iterations;
+        }
+
+        /// <summary>
+        /// Calculates approximate value of Pi. The algorithm is
+        /// incremental, i.e. the approximate value get better and
+        /// better, the longer the algorithm runs.
+        /// Note that this version of the algorithm is async.
+        /// </summary>
+        /// <param name="data">Reference to data coordination object</param>
+        /// <returns>Final value of Pi, if algorithm is allowed to run to completion</returns>
+        public async Task<double> CalculateAsync(PiCalcData data)
+        {
+            // Setup 
+            Random _generator = new Random();
+            int insideUnitCircle = 0;
+            double piCurrent = 0.0;
+
+            // Main loop in algorithm is started as a new Task
+            // Note that the task is awaited, i.e. the flow-of-execution
+            // returns to the caller of CalculateAsync.
+            await Task.Run(() =>
+            {
+                for (long i = 1; i <= 1000000000000 && !data.Quit; i++)
+                {
+                    insideUnitCircle += RandomPointWithinUnitCircle(_generator) ? 1 : 0;
+
+                    // Update data object
+                    piCurrent = insideUnitCircle * 4.0 / i;
+                    data.Pi = piCurrent;
+                    data.Iterations = i;
+                }
+            });
+
+            // Execution only reaches this point, if the user 
+            // did not interrupt the calculation.
+            return piCurrent;
+        }
+
+        /// <summary>
+        /// Generates a random point within the unit square,
+        /// and return whether or not the point was within
+        /// the unit circle
+        /// </summary>
+        private bool RandomPointWithinUnitCircle(Random generator)
+        {
+            // Create random point inside the unit square
+            double x = generator.NextDouble();
+            double y = generator.NextDouble();
+
+            return (x * x + y * y < 1.0);
+        }
+    }
+}
